303a2826e90de85ea47d4106a8a0217b34d64e7b
Sathira Tennakoon <65024183+sathira10@users.noreply.github.com>
2023-08-09 11:59:11 +0530

    next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
-  basePath: '/pv-emulator-frontend',
-  assetPrefix: '/pv-emulator-frontend',
+  // basePath: '/pv-emulator-frontend',
+  images: { unoptimized: true }
 }
 
 module.exports = nextConfig
